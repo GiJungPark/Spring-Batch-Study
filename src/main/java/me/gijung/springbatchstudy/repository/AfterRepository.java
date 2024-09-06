692451259ab6f0513943370ad9333f4b1b3b6ae6
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-09-06 16:38:44 +0900

    feat: After 테이블 생성

---
+++ src/main/java/me/gijung/springbatchstudy/repository/AfterRepository.java
@@ -0,0 +1,8 @@
+package me.gijung.springbatchstudy.repository;
+
+import me.gijung.springbatchstudy.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+
+}
