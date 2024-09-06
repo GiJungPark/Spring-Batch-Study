70621d762da6faf78e63d98e87583c9cce6c548b
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-09-06 16:36:51 +0900

    feat: Before 테이블 생성

---
+++ src/main/java/me/gijung/springbatchstudy/repository/BeforeRepository.java
@@ -0,0 +1,8 @@
+package me.gijung.springbatchstudy.repository;
+
+import me.gijung.springbatchstudy.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+
+}
